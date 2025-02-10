b98c766919335016673499ce01c390be56143220
Cicek <ahmetcicek085@gmail.com>
2025-02-10 17:53:24 +0100

    Added HTTP server with basic routing

--- Server/HttpServer.cs
+++ Server/HttpServer.cs
@@ -38,12 +38,41 @@ namespace MTCG.Server
 
             Console.WriteLine($"Anfrage erhalten:\n{request}");
 
-            // HTTP-Response bauen
-            string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, MTCG!";
+            // Die erste Zeile der HTTP-Anfrage auslesen (z. B. "GET /users HTTP/1.1")
+            string[] requestLines = request.Split("\r\n");
+            string[] requestParts = requestLines[0].Split(" ");
+            if (requestParts.Length < 2)
+            {
+                client.Close();
+                return;
+            }
+
+            string method = requestParts[0];  // GET, POST, PUT, DELETE
+            string path = requestParts[1];    // /users, /cards, etc.
+
+            string response = RouteRequest(method, path);
+
             byte[] responseData = Encoding.UTF8.GetBytes(response);
             stream.Write(responseData, 0, responseData.Length);
-
             client.Close();
         }
+
+        private string RouteRequest(string method, string path)
+        {
+            if (method == "GET" && path == "/users")
+            {
+                return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nListe der User";
+            }
+            else if (method == "POST" && path == "/users")
+            {
+                return "HTTP/1.1 201 Created\r\nContent-Type: text/plain\r\n\r\nUser erstellt";
+            }
+            else if (method == "GET" && path == "/cards")
+            {
+                return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nListe der Karten";
+            }
+            return "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nRoute nicht gefunden";
+        }
+
     }
 }
